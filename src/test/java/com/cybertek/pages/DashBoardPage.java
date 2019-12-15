db56c4f511b4446487294d23bba94f5435252e2d
juxel <yspostbox@yahoo.com>
2019-12-15 21:02:03 +0100

    new

---
+++ src/test/java/com/cybertek/pages/DashBoardPage.java
@@ -0,0 +1,18 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashBoardPage extends BasePage{
+
+   /*
+  - we dont need constructor below, because it will use parents constructor
+  - kalsa da olur because it will override
+    public DashBoardPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+    */
+
+
+}
